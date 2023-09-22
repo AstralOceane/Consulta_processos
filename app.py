642088bd9e56e8fa1363e3a8124925234ac27b3f
AstralOceane <matheus.novaes998@gmail.com>
2023-09-22 17:56:47 -0300

    debugado e corrigido

--- app.py
+++ app.py
@@ -1,98 +1,78 @@
 from selenium import webdriver
-#web driver permite fazer as automações
 from selenium.webdriver.common.keys import Keys
-#Keys permite simular o uso do teclado
 from selenium.webdriver.common.by import By
-#By permite encontar elementos
 from selenium.webdriver.support.select import Select
-#Select permite trabalhar com drop downs
 from time import sleep
-# Sleep para que possa dar uma pausa quando necessario
 import openpyxl
+
 numero_oab = 133864
-# entrar no site da - https://pje-consulta-publica.tjmg.jus.br/
+
+# Inicializar o WebDriver do Chrome
 driver = webdriver.Chrome()
 driver.get('https://pje-consulta-publica.tjmg.jus.br/')
 sleep(15)
-# digitar número oab
-# para encontrar um campo atraves do Selenium voce precisa realizar um XPATH que basicamente é //tag[@atributo='valor'] exemplo //input[@id='fPP:Decoration:numeroOAB']
-# ou seja estou dizendo, procure na tag input, o atributo id, fPP:Decoration:numeroOAB
-# tag seria por exemplo, div, input, style, script. Já o atributo seria, class, id, type, maxlegh.
-campo_oab = driver.find_element(By.XPATH,"//input[@id='fPP:Decoration:numeroOAB']")
+
+# Digitar o número da OAB
+campo_oab = driver.find_element(By.XPATH, "//input[@id='fPP:Decoration:numeroOAB']")
 campo_oab.send_keys(numero_oab)
-#selecionar estado
-dropdown_estados = driver.find_element(By.XPATH,"//select[@id='fPP:Decoration:estadoComboOAB']")
+
+# Selecionar o estado
+dropdown_estados = driver.find_element(By.XPATH, "//select[@id='fPP:Decoration:estadoComboOAB']")
 opcoes_estados = Select(dropdown_estados)
 opcoes_estados.select_by_visible_text('SP')
-3 # clicar em pesquisar
+
+# Clicar em pesquisar
 botao_pesquisar = driver.find_element(By.XPATH, "//input[@id='fPP:searchProcessos']")
 botao_pesquisar.click()
 sleep(10)
-#entrar em cada um dos processsos
-processos = driver.find_elements(By.XPATH,"//b[@class='btn-block']")
+
+# Entrar em cada um dos processos
+processos = driver.find_elements(By.XPATH, "//b[@class='btn-block']")
+
+# Carregar a planilha ou criar uma nova se não existir
+try:
+    workbook = openpyxl.load_workbook('dados.xlsx')
+except FileNotFoundError:
+    workbook = openpyxl.Workbook()
+
 for processo in processos:
     processo.click()
     sleep(10)
     janelas = driver.window_handles
     driver.switch_to.window(janelas[-1])
-    driver.set_window_size(1920,1080)
-    # extrair o n° do processo e data da distribuição
-    numero_processo = driver.find_elements(By.XPATH,"//div[@class='col-sm-12 ']")
-    numero_processo = numero_processo[0]
-    numero_processo = numero_processo.text
+    driver.set_window_size(1920, 1080)
 
-    data_distribuicao = driver.find_elements(By.XPATH,"//div[@class='value col-sm-12 ']")
-    data_distribuicao = data_distribuicao[1]
-    data_distribuicao = data_distribuicao.text
-    # extrair e guardar todas as últimas movimentaç
-#movimentações
-    movimentacoes = driver.find_elements(By.XPATH,"//div[@id='j_id132:processoEventoPanel_body']//tr[contains(@class,'rich-table-row')]//td//div//div//span")
-    lista_movimentacoes = []
-    for movimentacao in movimentacoes:
-        lista_movimentacoes.append(movimentacao.text)
-    
-#dessa vez queremos que ele entre em um processo, feche e entre em outro, e repita o processo, até abrir todos.
+    # Extrair informações do processo
+    numero_processo_element = driver.find_element(By.XPATH, "//div[@class='col-sm-12 ']")
+    data_distribuicao_element = driver.find_element(By.XPATH, "//div[@class='value col-sm-12 ']")
 
-# guardar tudo no excel, separados por processo
+    numero_processo = numero_processo_element.text
+    data_distribuicao = data_distribuicao_element.text
 
-workbook = openpyxl.load_workbook('dados.xlsx')
-try: 
-    #código para inserir dados em página existente e inserir as informações
-    #acessasr página do processo
-    pagina_processo = workbook[numero_processo]
-    #criar nome das colunas
-    pagina_processo['A1'].value = "Número Processo"
-    pagina_processo['B1'].value = "Data de Distribuição"
-    pagina_processo['C1'].value = "Movimentações"
-    #adicionar numero do processo
-    pagina_processo['A2'].value = numero_processo
-    #adicionar data de distribuição
-    pagina_processo['B2'].value = data_distribuicao
-    #adicionar movimentações
-    for index, row in enumerate(pagina_processo.iter_rows(min_row=2,max_row=len(lista_movimentacoes),min_col=3,max_col=3)):
-        for cell in row:
-            cell.value = lista_movimentacoes[index]
+    movimentacoes = driver.find_elements(By.XPATH, "//div[@id='j_id132:processoEventoPanel_body']//tr[contains(@class,'rich-table-row')]//td//div//div//span")
+    lista_movimentacoes = [movimentacao.text for movimentacao in movimentacoes]
+
+    # Criar uma nova planilha para o processo ou acessar uma existente
+    try:
+        pagina_processo = workbook[numero_processo]
+    except KeyError:
+        pagina_processo = workbook.create_sheet(numero_processo)
+
+    # Inserir os dados na planilha
+    pagina_processo['A1'] = "Número Processo"
+    pagina_processo['B1'] = "Data de Distribuição"
+    pagina_processo['C1'] = "Movimentações"
+    pagina_processo['A2'] = numero_processo
+    pagina_processo['B2'] = data_distribuicao
+
+    for index, movimentacao in enumerate(lista_movimentacoes, start=3):
+        pagina_processo.cell(row=index, column=3, value=movimentacao)
+
+    # Salvar a planilha
     workbook.save('dados.xlsx')
     driver.close()
     sleep(5)
-    driver.switch_to.window(driver.window_handles[0])        
-except Exception as error:       
-    #criar uma pagina do zero
-    workbook.create_sheet(numero_processo)
-    pagina_processo = workbook[numero_processo]
-    #criar nome das colunas
-    pagina_processo['A1'].value = "Número Processo"
-    pagina_processo['B1'].value = "Data de Distribuição"
-    pagina_processo['C1'].value = "Movimentações"
-    #adicionar numero do processo
-    pagina_processo['A2'].value = numero_processo
-    #adicionar data de distribuição
-    pagina_processo['B2'].value = data_distribuicao
-    #adicionar movimentações
-    for index, row in enumerate(pagina_processo.iter_rows(min_row=2,max_row=len(lista_movimentacoes),min_col=3,max_col=3)):
-        for cell in row:
-            cell.value = lista_movimentacoes[index]
-    workbook.save('dados.xlsx')
-    driver.close()
-    sleep(5)
-    driver.switch_to.window(driver.window_handles[0])     
+    driver.switch_to.window(driver.window_handles[0])
+
+# Fechar o WebDriver
+driver.quit()
